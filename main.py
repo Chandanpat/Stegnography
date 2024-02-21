8944086f742a5b050765ef9a52e6fc8e11b6d06a
TejasBarge02 <barge237@gmail.com>
2024-02-21 07:51:10 +0530

    image in image fixed, image in video done (format to be fixed), video in video in progress

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import TextInText as TinT
+import text_in_text as TinT
 import text_in_image as TinI
-import LSB_Img_in_Img as IinI
-import txtinaudio as TinA
+import image_in_image as IinI
+import text_in_audio as TinA
 # import audimg as AinI
 import audio_in_image as AinI
 import text_in_video as TinV
+import image_in_video as IinV
 
 
 print("\n\n\t\t\t\t\t#####  Welcome to Multimedia Steganography Tool  #####\n\n")
 while True:
-   print("\n\n\t1. Text in Text Steganography\n\t2. Text in Image Steganography\n\t3. Image in Image Steganography\n\t4. Audio in Image Steganography\n\t5. Text in Audio Steganography\n\t6. Text in Video Steganography\n\t7. Exit")
+   print("\n\n\t1. Text in Text Steganography\n\t2. Text in Image Steganography\n\t3. Image in Image Steganography\n\t4. Audio in Image Steganography\n\t5. Text in Audio Steganography\n\t6. Text in Video Steganography\n\t7. Image in Video Steganography\n\t8. Exit")
    ch = int(input("\n\t\tChoose from below options: \n"))
 
    if ch == 1:
@@ -29,8 +30,11 @@ while True:
 
    elif ch == 6:
         TinV.caller()
-   
+
    elif ch == 7:
+        IinV.caller()
+   
+   elif ch == 8:
         print("\n\nExiting.....")
         break
    else:
